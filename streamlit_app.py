d9622553e83f7aa692bdea0c26778b535d43f8ea
tuyennguyen02181997 <tuyen.nguyen@manpowergroup.com>
2025-04-28 10:31:16 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,6 +36,9 @@ if ingredients_list:
             values ('""" + ingredients_string + """','""" + name_on_order + """')"""
 
 
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+#st.text(smoothiefroot_response)
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
 
     #st.write(my_insert_stmt)
